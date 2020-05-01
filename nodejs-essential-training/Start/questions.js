3b5ca5f7b397919235a9f2fe76502ed9cdea7797
gautamn <navin.gautam@tabcorp.com.au>
2020-05-01 17:50:27 +1000

    improve collectAnswers module with eventEmitter

--- nodejs-essential-training/Start/questions.js
+++ nodejs-essential-training/Start/questions.js
@@ -6,8 +6,20 @@ const questions = [
     "What are you going to do with node js? "
 ];
 
-collectAnswers(questions, answers => {
+const answerEvents = collectAnswers(questions, answers => {
     console.log("Thank you for your answers. ");
     console.log(answers);
     process.exit();
 });
+
+answerEvents.on("answer", answer =>
+    console.log(`question answered: ${answer}`)
+);
+
+answerEvents.on("complete", answers => {
+    console.log("Thank you for your answers. ");
+    console.log(answers);
+
+});
+
+answerEvents.on("complete", () => process.exit());
